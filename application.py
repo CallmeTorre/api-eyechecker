37ea96ccdac52d053c8af0a76fd36a0bdd33f87a
CallmeTorre <alexis_torreblanca@outlook.com>
2019-06-14 12:13:54 -0500

    Added new patient endpoint

--- application.py
+++ application.py
@@ -1,10 +1,15 @@
 from os import getenv
 from traceback import format_exc
 
+from flask.views import MethodView
 from flask import (Flask,
                    jsonify,
                    make_response,
                    request)
+
+from eyechecker.utils.validation import validate_params
+from eyechecker.utils.schemas import newpatientschema
+from eyechecker.utils.command import Command
 
 application = Flask(__name__)
 
@@ -18,6 +23,30 @@ def root():
     return make_response(
         jsonify(
             {'status': 'ok'}), 200)
+
+class NewPacient(MethodView):
+    """
+    Class that manages the creation of a new pacient.
+    """
+
+    decorators = [validate_params(newpatientschema)]
+    def post(self, params):
+        command = Command(params)
+        command.execute("newpatientresponse")
+        return make_response(
+            jsonify(command.result),
+            command.status)
+
+
+new_patient_view = NewPacient.as_view('newpatient')
+
+application.add_url_rule(
+    '/patient/new',
+    view_func=new_patient_view,
+    methods=[
+        'POST',
+    ]
+)
 
 @application.errorhandler(500)
 def internal_error(error):
