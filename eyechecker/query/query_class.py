71e9625bc21414a6ddeb4b56af2fd7a19e11f990
CallmeTorre <alexis_torreblanca@outlook.com>
2019-06-14 16:46:41 -0500

    Added patients index query

--- eyechecker/query/query_class.py
+++ eyechecker/query/query_class.py
@@ -1,4 +1,7 @@
+from sqlalchemy.sql import select, and_
+
 from eyechecker.information.information_class import Information
+from eyechecker.filter.filter_class import Filter
 
 class Query:
     """
@@ -7,9 +10,24 @@ class Query:
 
     def __init__(self):
         self.information = Information()
+        self.filter = Filter()
 
     def newpatientquery(self, params):
         """
         Query used to insert a new patient information.
         """
         return self.information.patient_table.insert().values(**params)
+
+    def _patientindexbasequery(self, params):
+        """
+        Base query used to get all the patients given a specific filter.
+        """
+        return select([self.information.patientindexview_table]).\
+            where(and_(
+                *self.filter.patientindexfilter(
+                    params)))
+
+    def patientindexquery(self, params):
+        return self._patientindexbasequery(params).\
+            order_by(*self.filter.orderargument(
+                params))
