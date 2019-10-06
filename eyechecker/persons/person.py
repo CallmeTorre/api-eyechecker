df7f988491bd98c2e56e69141426f05d6ac7595b
CallmeTorre <alexis_torreblanca@outlook.com>
2019-10-05 22:04:09 -0500

    Fixed patient operations

--- eyechecker/persons/person.py
+++ eyechecker/persons/person.py
@@ -1,6 +1,9 @@
 from abc import ABCMeta, abstractmethod, abstractproperty
 
+from sqlalchemy import Table
+
 from eyechecker.utils.connection import engine, meta
+from eyechecker.utils.formatter import format_person
 
 class Person(metaclass=ABCMeta):
 
@@ -9,6 +12,11 @@ class Person(metaclass=ABCMeta):
         self._engine = engine
         self._meta = meta
         self._params = params
+        self._persons = Table(
+            'personas',
+            self.meta,
+            autoload=True,
+            autoload_with=self.engine)
 
     @property
     def engine(self):
@@ -18,9 +26,24 @@ class Person(metaclass=ABCMeta):
     def meta(self):
         return self._meta
 
+    @property
+    def persons(self):
+        return self._persons
+
     @abstractproperty
     def table(self):
         pass
+
+    def _insert_person(self):
+        person = format_person(self._params)
+        try:
+            id_person = self.engine.execute(
+                self.persons.insert().values(**person)).inserted_primary_key[0]
+            return id_person
+        except Exception as e:
+            logging.error("No se puedo crear la persona")
+            logging.exception(str(e))
+            return None
 
     @abstractmethod
     def create(self):
---
+++ eyechecker/utils/formatter.py
@@ -0,0 +1,23 @@
+def format_person(params):
+    return {
+        'nombre': params['nombre'],
+        'apellido_paterno': params['apellido_paterno'],
+        'apellido_materno': params['apellido_materno'],
+        'fecha_nacimiento': params['fecha_nacimiento'],
+        'email': params['email'],
+        'telefono_celular': params['telefono_celular'],
+        'genero': params['genero']
+    }
+
+
+def format_patient(params):
+    return {
+        'id_persona': params['id_persona'],
+        'curp': params['curp'],
+        'id_ocupacion': params['ocupacion'],
+        'id_estado_civil': params['estado_civil'],
+        'enfermedades_recientes': params['enfermedades_recientes'],
+        'medicamentos': params['medicamentos'],
+        'enfermedades_cronicas': params['enfermedades_cronicas'],
+        'enfermedades_hereditarias': params['enfermedades_hereditarias']
+    }
